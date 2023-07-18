1d78fb8fadd7145355e0db7c654aaa3e707b22e5
RA4Z <rraz639@gmail.com>
2023-07-18 16:18:27 -0300

    Usando StreamReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,6 +5,25 @@ partial class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+
+        using(var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+            //var linha = leitor.ReadLine();    LÊ A PRIMEIRA LINHA DO ARQUIVO
+
+            //var texto = leitor.ReadToEnd();   LÊ O ARQUIVO INTEIRO DE UMA VEZ
+
+            //var numero = leitor.Read();       TRAZ O PRIMEIRO CÓDIGO DO ARQUIVO
+
+            while(!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+
+        }
         Console.ReadLine();
     }
 
