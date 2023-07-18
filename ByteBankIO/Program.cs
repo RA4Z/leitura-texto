abe3c3e445eae47bebcffa011e47ee82535e1722
RA4Z <rraz639@gmail.com>
2023-07-18 15:53:57 -0300

    Implementando close e using

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,20 +6,21 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        var buffer = new byte[1024];    // 1KB
-
-        while(numeroDeBytesLidos != 0)
+        using(var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            var numeroDeBytesLidos = -1;
+            var buffer = new byte[1024];    // 1KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
         }
-
-        // public override int Read(byte[] array, int offset, int count);
-
-        Console.ReadLine();
     }
 
     static void EscreverBuffer(byte[] buffer)
