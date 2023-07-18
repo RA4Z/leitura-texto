f09dc0a8458d6c263ede73dc06a141039fd4372c
RA4Z <rraz639@gmail.com>
2023-07-18 16:01:50 -0300

    Trabalhando com buffers

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -15,7 +15,7 @@ class Program
             while (numeroDeBytesLidos != 0)
             {
                 numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
             }
 
             fluxoDoArquivo.Close();
@@ -23,10 +23,12 @@ class Program
         }
     }
 
-    static void EscreverBuffer(byte[] buffer)
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
         var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer);
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+
         Console.Write(texto);
         /*
         foreach (byte b in buffer)
