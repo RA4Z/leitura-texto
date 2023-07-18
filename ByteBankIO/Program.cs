ea0e6faa0af405eaaeaa7e5660eee4136ffecda8
RA4Z <rraz639@gmail.com>
2023-07-18 14:31:36 -0300

    Imprimindo na tela todas as informações do TXT

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,7 +5,28 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+        var buffer = new byte[1024];    // 1KB
+
+        while(numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        // public override int Read(byte[] array, int offset, int count);
 
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (byte b in buffer)
+        {
+            Console.Write(b);
+            Console.Write(' ');
+        }
     }
 }
