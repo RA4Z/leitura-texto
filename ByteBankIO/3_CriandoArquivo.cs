e38cc3b2b9e10c02bc087e4eea11b880e5fa78f2
RA4Z <rraz639@gmail.com>
2023-07-19 16:32:40 -0300

    Usando o método Flush para escrever em tempo real os dados em um arquivo de texto

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -23,12 +23,29 @@ partial class Program
         //FileMode.Create SUBSTITUI O ARQUIVO NO CAMINHO
         //FileMode.CreateNew SOMENTE CRIA UM NOVO ARQUIVO CASO O MESMO NÃO EXISTA NO DIRETÓRIO ALVO
         var caminhoNovoArquivo = "contasExportadas.csv";
+
         using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
         using (var escritor = new StreamWriter(fluxoDeArquivo))
         {
             escritor.Write("456, 7895, 4789.40, Robert Zimmermann");
         }
+    }
+    static void TestaEscrita()
+    {
+        var caminhoNovoArquivo = "teste.txt";
 
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                escritor.WriteLine($"Linha {i}");
+                escritor.Flush();   //Despeja o buffer para o Stream
+
+                Console.WriteLine($"Linha {i} foi escrita no arquivo .Tecle enter...");
+                Console.ReadLine();
+            }
+        }
 
     }
 }
