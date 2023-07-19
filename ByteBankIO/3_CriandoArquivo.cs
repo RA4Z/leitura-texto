7ac036ea50a6db188e1106fedfde2e27b43e8c62
RA4Z <rraz639@gmail.com>
2023-07-19 07:27:11 -0300

    Usando StreamWriter

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -7,7 +7,7 @@ partial class Program
     {
         var caminhoNovoArquivo = "contasExportadas.csv";
 
-        using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
         {
             var contaComoString = "456, 7895, 4785.40, Gustavo Santos";
             
@@ -17,5 +17,18 @@ partial class Program
 
             fluxoDeArquivo.Write(bytes, 0, bytes.Length);
         }
+    }
+    static void CriarArquivoComWriter()
+    {
+        //FileMode.Create SUBSTITUI O ARQUIVO NO CAMINHO
+        //FileMode.CreateNew SOMENTE CRIA UM NOVO ARQUIVO CASO O MESMO NÃO EXISTA NO DIRETÓRIO ALVO
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("456, 7895, 4789.40, Robert Zimmermann");
+        }
+
+
     }
 }
