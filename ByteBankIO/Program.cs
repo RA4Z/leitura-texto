a7505de743bc25729fc67790c444d5bc11ec9b99
RA4Z <rraz639@gmail.com>
2023-07-19 16:36:40 -0300

    Começando a escrever textos binários

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,8 +5,18 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        TestaEscrita();
+        var caminhoNovoArquivo = "TestaEscrita.txt";
 
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.WriteLine(true);
+            escritor.WriteLine(false);
+            escritor.WriteLine(45454545454545);
+        }
+
+        Console.WriteLine("Aplicação Finalizada...");
+        
         Console.ReadLine();
     }
 }
