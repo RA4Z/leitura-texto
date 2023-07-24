ddef7fb2b87a8e9db9d9db67279dde6f655d43bc
RA4Z <rraz639@gmail.com>
2023-07-24 09:36:21 -0300

    Criando função para leitura de informações binárias

--- ByteBankIO/4_StreamBinario.cs
+++ ByteBankIO/4_StreamBinario.cs
@@ -13,4 +13,18 @@ partial class Program
             escritor.Write("Robert Zimmermann");
         }
     }
+
+    static void LeituraBinaria()
+    {
+        using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+        using (var leitor = new BinaryReader(fs))
+        {
+            var agencia = leitor.ReadInt32();
+            var numeroConta = leitor.ReadInt32();
+            var saldo = leitor.ReadDouble();
+            var titular = leitor.ReadString();
+
+            Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+        }
+    }
 }
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,18 +5,10 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var caminhoNovoArquivo = "TestaEscrita.txt";
+        EscritaBinaria();
+        LeituraBinaria();
 
-        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-        using (var escritor = new StreamWriter(fluxoDeArquivo))
-        {
-            escritor.WriteLine(true);
-            escritor.WriteLine(false);
-            escritor.WriteLine(45454545454545);
-        }
-
-        Console.WriteLine("Aplicação Finalizada...");
-        
+        Console.WriteLine("Aplicação Finalizada");
         Console.ReadLine();
     }
 }
