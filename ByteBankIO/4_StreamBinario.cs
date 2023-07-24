e6f9839c15bf1f87278ad556ac0525cb96312dda
RA4Z <rraz639@gmail.com>
2023-07-24 09:29:06 -0300

    Adicionando arquivo de binários

---
+++ ByteBankIO/4_StreamBinario.cs
@@ -0,0 +1,16 @@
+﻿using ByteBankIO;
+using System.Text;
+partial class Program
+{
+    static void EscritaBinaria()
+    {
+        using (var fs = new FileStream("contaCorrente.txt", FileMode.Create))
+        using (var escritor = new BinaryWriter(fs))
+        {
+            escritor.Write(456);
+            escritor.Write(546544);
+            escritor.Write(4000.50);
+            escritor.Write("Robert Zimmermann");
+        }
+    }
+}
