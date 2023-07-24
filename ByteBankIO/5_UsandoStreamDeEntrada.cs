2fe372127207d3612f768101b98cfbe1095ba8e6
RA4Z <rraz639@gmail.com>
2023-07-24 09:44:48 -0300

    Criando classe para usar stream como entrada

---
+++ ByteBankIO/5_UsandoStreamDeEntrada.cs
@@ -0,0 +1,15 @@
+﻿using ByteBankIO;
+using System.Text;
+partial class Program
+{
+    static void UsarStreamDeEntrada()
+    {
+        using (var fluxoDeEntrada = Console.OpenStandardInput())
+        {
+            var buffer = new byte[1024];
+            var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+            Console.WriteLine($"Bytes lidos na console: {bytesLidos}");
+        }
+    }
+}
